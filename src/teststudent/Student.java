d23edc29288160b309d785372e37c721c4c505b6
Viet Truong <truongcvd00176@fpt.edu.vn>
2017-08-26 10:47:17 +0700

    Them moi class  student

---
+++ src/teststudent/Student.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teststudent;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Student {
+    private String name;
+    private String email;
+    private String birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+    
+    
+    
+}
